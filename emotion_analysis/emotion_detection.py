bd41c2257779cece8ebf6a1c25300b6e1d5ad8b5
tintuthilak <tintu_thilak@yahoo.co.in>
2026-02-09 00:40:21 -0500

    emotion detection formatted response

--- emotion_analysis/emotion_detection.py
+++ emotion_analysis/emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze : str):
     """
@@ -11,5 +12,13 @@ def emotion_detector(text_to_analyze : str):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json = input_json, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    # Find the dominant action with highest score
+    max_key = max(emotions, key=emotions.get)
+    emotions["dominant_emotion"] = max_key
+
+    return emotions
 
